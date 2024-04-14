d8f9d6cabac8201fab3ab025b99d288074e0b5e1
h0z058l <h0z058l@sams.club.com>
2024-04-13 22:18:45 -0500

    create a separate folder for each idea

---
+++ BFW/3/create_data_file.py
@@ -0,0 +1,130 @@
+import json
+import os
+import random
+import numpy as np
+import h5py
+import skimage.io
+from skimage.transform import resize
+
+
+parent_path = '/Users/h0z058l/Downloads/FER/codes/Face-Recognition/BFW/3'
+
+"""
+-------- select 10 fold image paths ----------
+"""
+def select_images(path, num):
+    all_images = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith((".png", ".jpg", ".jpeg")):  # 检查文件类型，根据需要调整
+                all_images.append(os.path.join(root, file))
+
+    selected_images = random.sample(all_images, num)
+    remaining_images = list(set(all_images) - set(selected_images))
+
+    return selected_images, remaining_images
+
+
+def create_image_to_label_map(path):
+    selected_images, remaining_images = select_images(path, 250)
+
+    for train_image in remaining_images:
+        race_sex = train_image.split("/")[-3]
+        train_x.append(train_image)
+        train_y.append(race_sex)
+
+    for test_image in selected_images:
+        race_sex = test_image.split("/")[-3]
+        test_x.append(test_image)
+        test_y.append(race_sex)
+
+
+for idx in range(10):
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
+    img_parent_path = '/Users/h0z058l/Downloads/FER/dataset/BFW/bfw-cropped-aligned'
+    race_sex_category = ["asian_females", "asian_males", "black_females", "black_males",
+                         "indian_females", "indian_males", "white_females", "white_males"]
+    for race_sex in race_sex_category:
+        race_sex_path = os.path.join(img_parent_path, race_sex)
+        create_image_to_label_map(race_sex_path)
+
+    json_dict = {
+        "train_path": train_x, "train_label": train_y, "test_path": test_x, "test_label": test_y
+    }
+    with open(f"{parent_path}/10-folder-selection/select_{idx}.json", 'w') as f:
+        json.dump(json_dict, f)
+print("selection finished")
+
+
+"""
+------------ create data file -------------
+"""
+for select_idx in range(10):
+    train_pixels = []
+    test_pixels = []
+
+    with open(f"{parent_path}/10-folder-selection/select_{select_idx}.json") as f:
+        selection = json.load(f)
+
+    train_paths = selection["train_path"]
+    train_labels = selection["train_label"]
+    train_labels_set = set(train_labels)
+    train_labels_list = list(train_labels_set)
+    train_index_to_label_map = {}
+    train_label_to_index_map = {}
+    for train_label_idx, label in enumerate(train_labels_list):
+        train_index_to_label_map[train_label_idx] = label
+        train_label_to_index_map[label] = train_label_idx
+
+    test_paths = selection["test_path"]
+    test_labels = selection["test_label"]
+    test_labels_set = set(test_labels)
+    test_labels_list = list(test_labels_set)
+    test_index_to_label_map = {}
+    test_label_to_index_map = {}
+    for test_label_idx, label in enumerate(test_labels_list):
+        test_index_to_label_map[test_label_idx] = label
+        test_label_to_index_map[label] = test_label_idx
+
+    train_int_labels = []
+    for train_label in train_labels:
+        train_int_labels.append(train_label_to_index_map[train_label])
+    test_int_labels = []
+    for test_label in test_labels:
+        test_int_labels.append(test_label_to_index_map[test_label])
+
+    selection["train_idx2label_map"] = train_index_to_label_map
+    selection["train_label2idx_map"] = train_label_to_index_map
+    selection["test_idx2label_map"] = test_index_to_label_map
+    selection["test_label2idx_map"] = test_label_to_index_map
+    with open(f"{parent_path}/10-folder-selection/select_{select_idx}.json", 'w') as f:
+        json.dump(selection, f)
+    
+    for train_path in train_paths:
+        train_pixel = skimage.io.imread(train_path)
+        resized_image = resize(train_pixel, (108, 124))
+        train_pixels.append(resized_image.tolist())
+    for test_path in test_paths:
+        test_pixel = skimage.io.imread(test_path)
+        resized_image = resize(test_pixel, (108, 124))
+        test_pixels.append(resized_image.tolist())
+
+    train_pixels = np.array(train_pixels)
+    train_int_labels = np.array(train_int_labels)
+    test_pixels = np.array(test_pixels)
+    test_int_labels = np.array(test_int_labels)
+    print(np.shape(train_pixels))
+    print(np.shape(test_pixels))
+
+    datafile = h5py.File(f'{parent_path}/data/data_{select_idx}.h5', 'w')
+    datafile.create_dataset("Training_pixel", dtype='uint8', data=train_pixels)
+    datafile.create_dataset("Training_label", dtype='int64', data=train_int_labels)
+    datafile.create_dataset("test_pixels", dtype='uint8', data=test_pixels)
+    datafile.create_dataset("Test_label", dtype='int64', data=test_int_labels)
+    datafile.close()
+    print(f"{select_idx} data files created")
+
+print("Save data finish!!!")
---
+++ BFW/train_bfw.py
@@ -0,0 +1,222 @@
+# 10 crop for data enhancement
+from __future__ import print_function
+import torch.optim as optim
+import numpy as np
+import os
+import argparse
+import utils
+from BFW.bfw_dataset import BFW
+from models import *
+import torchvision.transforms as transforms
+import time
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description='PyTorch BFW CNN Training')
+parser.add_argument('--model', type=str, default='VGG19', help='CNN architecture')
+parser.add_argument('--dataset', type=str, default='BFW', help='CNN architecture')
+parser.add_argument('--bs', default=128, type=int, help='batch size')
+parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+opt = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+best_Test_acc = 0  # best Test accuracy
+best_Test_acc_epoch = 0
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+learning_rate_decay_start = 50  # 50
+learning_rate_decay_every = 5  # 5
+learning_rate_decay_rate = 0.9  # 0.9
+
+total_epoch = 10
+
+path = os.path.join(opt.dataset + '_' + opt.model)
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.Resize((108, 124)),  # 调整图片大小为 108x124
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+transform_test = transforms.Compose([
+    transforms.Lambda(lambda img: torch.stack([transforms.ToTensor()(img)])),
+])
+
+trainset = BFW(split='Training', transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=opt.bs, shuffle=True, num_workers=0)
+Testset = BFW(split='Test', transform=transform_test)
+Testloader = torch.utils.data.DataLoader(Testset, batch_size=opt.bs, shuffle=False, num_workers=0)
+
+# Model
+if opt.model == 'VGG19':
+    net = VGG('VGG19')
+elif opt.model == 'Resnet18':
+    net = ResNet18()
+
+if opt.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir(path), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load(os.path.join(path, 'Test_model.t7'))
+
+    net.load_state_dict(checkpoint['net'])
+    best_Test_acc = checkpoint['best_Test_acc']
+    best_Test_acc_epoch = checkpoint['best_Test_acc_epoch']
+    start_epoch = checkpoint['best_Test_acc_epoch'] + 1
+else:
+    print('==> Building model..')
+
+if use_cuda:
+    print('==> Using CUDA...')
+    net.cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=opt.lr, momentum=0.9, weight_decay=5e-4)
+
+filename = time.strftime("%m-%d-%H-%M-%S", time.localtime())
+log_path = "/Users/h0z058l/Downloads/FER/codes/Face-Recognition/output/" + f'{filename}.txt'  # 也可以创建一个.doc的word文档
+outputfile = open(log_path, 'w')
+
+Loss_list = []
+Train_acc_list = []
+
+
+# Training
+def train(epoch):
+    print(f"Epoch: {epoch}", )
+    print(f"Epoch: {epoch}", file=outputfile)
+
+    global Train_acc
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+
+    if epoch > learning_rate_decay_start and learning_rate_decay_start >= 0:
+        frac = (epoch - learning_rate_decay_start) // learning_rate_decay_every
+        decay_factor = learning_rate_decay_rate ** frac
+        current_lr = opt.lr * decay_factor
+        utils.set_lr(optimizer, current_lr)  # set the decayed rate
+    else:
+        current_lr = opt.lr
+    print('learning_rate: %s' % str(current_lr))
+
+    for batch_idx, (inputs, targets) in enumerate(tqdm(trainloader)):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        optimizer.zero_grad()
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        utils.clip_gradient(optimizer, 0.1)
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        if batch_idx % 25 == 0:
+            print("Loss:", "{:.4e}".format(loss))
+            print(f"Correct_cnt: {correct}")
+            print("Loss:", "{:.4e}".format(loss), file=outputfile)
+            print(f"Correct_cnt: {correct}", file=outputfile)
+        outputfile.flush()
+
+        utils.progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                           % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+    Train_acc = 100. * correct / total
+
+
+Test_acc_list = []
+
+
+def test(epoch):
+    global Test_acc
+    global best_Test_acc
+    global best_Test_acc_epoch
+    net.eval()
+    Test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(Testloader):
+        bs, ncrops, c, h, w = np.shape(inputs)
+        inputs = inputs.view(-1, c, h, w)
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        with torch.no_grad():
+            inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)
+        outputs_avg = outputs.view(bs, ncrops, -1).mean(1)  # avg over crops
+        loss = criterion(outputs_avg, targets)
+        Test_loss += loss.item()
+        _, predicted = torch.max(outputs_avg.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        utils.progress_bar(batch_idx, len(Testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                           % (Test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+    # Save checkpoint.
+    Test_acc = 100. * correct / total
+
+    if Test_acc > best_Test_acc:
+        print('Saving..')
+        print("best_Test_acc: %0.3f" % Test_acc)
+        state = {
+            'net': net.state_dict() if use_cuda else net,
+            'best_Test_acc': Test_acc,
+            'best_Test_acc_epoch': epoch,
+        }
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        torch.save(state, os.path.join(path, 'Test_model.t7'))
+        best_Test_acc = Test_acc
+        best_Test_acc_epoch = epoch
+
+
+for epoch in range(start_epoch, total_epoch):
+    train(epoch)
+    test(epoch)
+
+"""
+------------------ plot figures --------------------
+"""
+x1 = range(0, 10)
+y1 = Train_acc_list
+print(y1)
+print(y1, file=outputfile)
+plt.plot(x1, y1, '-')
+plt.title('Train_acc vs. epoches')
+plt.ylabel('Train_acc')
+plt.plot(x1, y1, color='blue', label='Train_acc')
+plt.savefig(f"/Users/h0z058l/Downloads/FER/codes/Face-Recognition/output/Train-acc-{filename}.png")
+
+x2 = range(0, 10)
+y2 = Loss_list
+print(y2)
+plt.plot(x2, y2, '-')
+plt.title('Train_loss vs. epoches')
+plt.xlabel('Epoch')
+plt.ylabel('Train_loss')
+plt.plot(x2, y2, color='blue')
+plt.savefig(f"/Users/h0z058l/Downloads/FER/codes/Face-Recognition/output/Train-loss-{filename}s.png")
+
+x3 = range(0, 10)
+y3 = Test_acc_list
+print(y3)
+print(y3, file=outputfile)
+plt.plot(x3, y3, '-')
+plt.title('Test_acc vs. epoches')
+plt.xlabel('Epoch')
+plt.ylabel('Test_acc')
+plt.plot(x3, y3, color='orange', label='Private_acc')
+plt.legend(loc='lower right')
+plt.savefig(f"/Users/h0z058l/Downloads/FER/codes/Face-Recognition/output/Test-acc-{filename}.png")
+plt.close()
+
+outputfile.close()
+print("best_Test_acc: %0.3f" % best_Test_acc)
+print("best_Test_acc_epoch: %d" % best_Test_acc_epoch)
